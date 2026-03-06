1df22256d1f301e9c41a912fd02aca96adc396a1
Yura <yurasmirnovlynxtram@gmail.com>
2026-03-06 09:02:30 +0900

    [ADD]: style in CourseCardsList

--- src/widgets/CourseCardsList/ui/CourseCardsList.tsx
+++ src/widgets/CourseCardsList/ui/CourseCardsList.tsx
@@ -4,7 +4,9 @@ import type { CourseCardProps } from "@entities/CourseCard/model/types";
 import { CourseCardModal } from "@entities/CourseCard/ui/components/CourseCardModal";
 import { Button } from "@shared/components/Button";
 import { useMemo, useState } from "react";
-import { useSearchParams } from "react-router-dom"
+import { useSearchParams } from "react-router-dom";
+
+import css from './index.module.scss';
 
 export const CourseCardsList = () =>  {
   const [searchParams, setSearchParams] = useSearchParams();
@@ -30,33 +32,31 @@ export const CourseCardsList = () =>  {
 
 
   return (
-    <div>
-      <div>
-        {filteredCourses.map((course) => (
-          <CourseCard
-            key={course.id} 
-            {...course} 
-            onOpen={() => setSelectedCourse(course)} // Передаем объект курса в стейт
+    <div className={css.course_cards_list}>
+      {filteredCourses.map((course) => (
+        <CourseCard
+          key={course.id} 
+          {...course} 
+          onOpen={() => setSelectedCourse(course)} // Передаем объект курса в стейт
+        />
+      ))}
+      {selectedCourse && (
+        <CourseCardModal
+          isOpen={!!selectedCourse} 
+          onClose={() => setSelectedCourse(null)} 
+          {...selectedCourse} 
+        />
+      )}
+      {filteredCourses.length === 0 && (
+        <div className={css.course_cards_list_error}>
+          Курсов с такими параметрами не нашлось
+          <Button
+            buttonType="default"
+            text="Сбросить фильтры"
+            onClick={() => setSearchParams({})}
           />
-        ))}
-        {selectedCourse && (
-          <CourseCardModal
-            isOpen={!!selectedCourse} 
-            onClose={() => setSelectedCourse(null)} 
-            {...selectedCourse} 
-          />
-        )}
-        {filteredCourses.length === 0 && (
-          <div className={''}>
-            Курсов с такими параметрами не нашлось
-            <Button
-              buttonType="default"
-              text="Сбросить фильтры"
-              onClick={() => setSearchParams({})}
-            />
-          </div>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   )
 }
